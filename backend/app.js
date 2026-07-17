de8876c3fcc8b6929eff3afb1951b6c1615f4f49
Gayatri Chhetri <gayatrichhetri@gmail.com>
2026-07-17 08:51:35 +0545

    test

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,12 @@ import foodRoutes from "./src/routes/food.routes.js";
 import orderRoutes from "./src/routes/order.routes.js";
 const app = express();
 
+app.get("/api/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "Backend is working",
+  });
+});
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
